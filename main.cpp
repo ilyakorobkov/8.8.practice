795b2e3bbb69526ced0f2a66b6804e3343fa5ba8
ilyakorobkov <112753624+ilyakorobkov@users.noreply.github.com>
2024-07-23 19:06:02 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "snake.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    Snake window;
+    window.setWindowTitle("Snake");
+    window.show();
+
+    return app.exec();
+}
